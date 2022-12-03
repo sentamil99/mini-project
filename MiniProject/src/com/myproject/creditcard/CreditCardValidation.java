4b9d853ab2b070542ca6e18b03d9068dec932a13
sentamil <selvansenthamil095.com>
2022-12-03 14:12:17 +0530

    card

--- MiniProject/src/com/myproject/creditcard/CreditCardValidation.java
+++ MiniProject/src/com/myproject/creditcard/CreditCardValidation.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CreditCardValidation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// /TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter Credit Card No");
 		String card=sc.next();
